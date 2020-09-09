054697e3ecbba5b4e4b3a0092dbf46b3e52db102
Falk <julien.delusseau@gmail.com>
2020-09-09 12:28:29 +0200

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/database')
+const path = require('path')
 
 const app = express()
 
@@ -13,6 +14,14 @@ app.use(express.json({ extended: false }))
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/events', require('./routes/api/events'))
+
+// Serve static assets in production
+if (process.env.NODE.ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
